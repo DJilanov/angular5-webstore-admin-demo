527aec441b74d066c2524b8517aff60a86e71a93
Dimitar Jilanov <djilanov@gmail.com>
2017-02-22 17:46:42 +0200

    Birth of training_seo_admin

---
+++ src/app/app.routing.ts
@@ -0,0 +1,57 @@
+// Angular 2 Modules
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Admin Views
+import { AdminComponent } from './components/admin/admin.component';
+import { AdminMessagesComponent } from './components/admin-messages/admin_messages.component';
+import { AdminProductsComponent } from './components/admin-products/admin_products.component';
+import { AdminCategoriesComponent } from './components/admin-categories/admin_categories.component';
+import { AdminNavigationComponent } from './components/admin-navigation/admin_navigation.component';
+
+const appRoutes: Routes = [
+  { 
+    path: '', 
+    component: AdminComponent,
+    data: {
+      title: 'Admin'
+    }
+  }, { 
+    path: 'navigation', 
+    component: AdminNavigationComponent,
+    data: {
+      title: 'Navigation'
+    }
+  }, { 
+    path: 'products', 
+    component: AdminProductsComponent,
+    data: {
+      title: 'Products'
+    }
+  }, { 
+    path: 'categories', 
+    component: AdminCategoriesComponent,
+    data: {
+      title: 'Categories'
+    }
+  }, { 
+    path: 'messages', 
+    component: AdminMessagesComponent,
+    data: {
+      title: 'Messages'
+    }
+  }, { 
+    // TODO
+    path: 'orders', 
+    component: AdminComponent,
+    data: {
+      title: 'Orders'
+    }
+  }
+];
+
+export const appRoutingProviders: any[] = [
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
